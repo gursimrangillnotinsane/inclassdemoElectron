96e0abfc9395fce476b5ea4a92614df97f7ff51f
gursimrangillnotinsane <132660017+gursimrangillnotinsane@users.noreply.github.com>
2025-03-27 15:12:59 -0400

    fixed folder issue

--- ipcHandler.js
+++ ipcHandler.js
@@ -1,5 +1,5 @@
 //required imports
-const { ipcMain } = require('electron');
+const { app, ipcMain } = require('electron');
 const path = require('path')
 const fs = require('fs')
 
@@ -7,7 +7,18 @@ const fs = require('fs')
 // IPC (Inter-Process Communication) handler
 ipcMain.handle('create-file', async (event, data) => {
     if (!data || !data.title || !data.content) return false;
-    const filePath = path.join(__dirname, 'note', `${data.title}.txt`)
+    // to store data in UserData
+    //C:\Users\hp\AppData\Roaming
+    const folderName = path.join(app.getPath('userData'), 'note');
+    try {
+        if (!fs.existsSync(folderName)) {
+            fs.mkdirSync(folderName);
+        }
+    }
+    catch (err) {
+        console.log(err.message)
+    }
+    const filePath = path.join(folderName, `${data.title}.txt`)
     fs.writeFileSync(filePath, data.content);
     return { success: true, filePath }
 })
